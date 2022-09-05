fc36711372604d19f50e090d7a42217fe51eb19d
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-09-05 13:44:05 -0600

    add: spider-man themed workplace data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,29 @@
 INSERT INTO department (name)
 VALUES 
-('IT'),
-('Finance & Accounting'),
-('Sales & Marketing'),
-('Operations');
+('Friends'),
+('Superhero'),
+('Villan'),
+('Family');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Full Stack Developer', 95000, 1),
-('Software Engineer', 110000, 1),
-('Accountant', 65000, 2), 
-('Finanical Analyst', 125000, 2),
-('Marketing Coordindator', 75000, 3), 
-('Sales Lead', 55000, 3),
-('Project Manager', 112000, 4),
-('Operations Manager', 94000, 4);
+('Siper-Man', 95000, 2),
+('Doctor Octopus', 110000, 3),
+('Green Goblin', 65000, 3), 
+('Sandman', 125000, 3),
+('Spider-man Comic', 75000, 2), 
+('Aunt', 55000, 4),
+('Uncle', 112000, 4),
+('Girlfriend', 94000, 1);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Peter', 'Parker', 2, null),
-('Miles', 'Morales', 1, 1),
-('Mary', 'Jane', 4, null),
-('Uncle', 'Ben', 3, 3),
+('Peter', 'Parker', 1, 3),
+('Miles', 'Morales', 5, 5),
+('Mary', 'Jane', 8, null),
+('Uncle', 'Ben', 7, 5),
 ('Aunt', 'May', 6, null),
-('Otto', 'Octavius', 5, 5),
-('Norman', 'Osborn', 7, null),
-('Harry', 'Osborn', 8, 7);
+('Otto', 'Octavius', 2, null),
+('Norman', 'Osborn', 3, null),
+('Willan', 'Baker', 4, null);
